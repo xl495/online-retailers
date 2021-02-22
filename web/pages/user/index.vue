aa51ffeb5c1ffd527892b184de179b91a6d884c0
xl495 <453007797@qq.com>
2021-02-22 22:53:48 +0800

    init

---
+++ web/pages/user/index.vue
@@ -0,0 +1,155 @@
+<template>
+	<view>
+		<view class="bg">
+			<view class="user-info">
+				<u-image width="70rpx" height="70rpx" shape="circle" :src="userInfo.avatar"></u-image>
+				<view class="user-info__name u-line-1 u-p-l-12">{{ userInfo.name || '请登录' }}</view>
+				<view class="user-info__more"><u-icon name="arrow-right" size="28"></u-icon></view>
+			</view>
+		</view>
+		<view class="tool"></view>
+		<view class="suspension-wrap">
+			<view class="suspension">
+				<view class="Sign u-flex suspension-item"><u-button type="primary" @click="$u.route('pages/user/sign/sign')" >签到</u-button></view>
+				<view class="suspension-item u-flex-col">
+					<view class="title u-type-info">50</view>
+					<view class="title u-type-info">积分</view>
+				</view>
+				<view class="suspension-item u-flex-col">
+					<view class="title u-type-info">1</view>
+					<view class="title u-type-info">优惠券</view>
+				</view>
+			</view>
+			
+		</view>
+		<view class="order-wrap">
+			<u-cell-group :border="false"><u-cell-item :border-bottom="false" title="我的订单" value="更多"></u-cell-item></u-cell-group>
+			<u-grid :col="5">
+				<u-grid-item v-for="(item, index) in orderList" :key="index">
+					<u-icon color="#9999AA" :name="item.icon" :size="46"></u-icon>
+					<view class="u-m-t-24 u-tips-color">{{ item.title }}</view>
+				</u-grid-item>
+			</u-grid>
+		</view>
+		<view class="link-wrap">
+			<u-cell-group><u-cell-item :title="item.title" v-for="(item, index) in links" :key="index" :arrow="item.isLink"></u-cell-item></u-cell-group>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			userInfo: {
+				avatar: 'https://cdn.uviewui.com/uview/example/fade.jpg'
+			},
+			title: 'Hello',
+			orderList: [
+				{
+					title: '全部订单',
+					icon: 'order'
+				},
+				{
+					title: '待付款',
+					icon: 'calendar'
+				},
+				{
+					title: '待发货',
+					icon: 'gift'
+				},
+				{
+					title: '待收货',
+					icon: 'car'
+				},
+				{
+					title: '退款/售后',
+					icon: 'error-circle'
+				}
+			],
+			links: [
+				{
+					title: '购物车',
+					icon: 'hourglass',
+					isLink: true,
+					path: ''
+				},
+				{
+					title: '地址管理',
+					icon: 'hourglass',
+					isLink: true,
+					path: ''
+				},
+				{
+					title: '在线客服',
+					icon: 'hourglass',
+					isLink: true,
+					path: ''
+				},
+				{
+					title: '退出登录',
+					icon: 'hourglass',
+					isLink: false,
+					path: ''
+				}
+			]
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+page {
+	position: relative;
+	.user-info {
+		display: flex;
+		color: white;
+		.user-info__name {
+			flex: 1;
+			font-size: 34rpx;
+			line-height: 70rpx;
+		}
+		.user-info__more {
+			display: flex;
+			align-items: center;
+		}
+		padding: 24rpx;
+	}
+	.bg {
+		height: 300rpx;
+		background-color: #464c5b;
+	}
+	.suspension-wrap {
+		position: absolute;
+		top: 225rpx;
+		left: 48rpx;
+		right: 48rpx;
+		height: 150rpx;
+		background-color: white;
+		z-index: 10;
+		padding: 24rpx 0;
+		.suspension {
+			display: flex;
+			.suspension-item {
+				&:nth-child(1),&:nth-child(2) {
+					border-right: 2rpx solid #e4e7ed;
+				}
+				flex: 1;
+				.title {
+					line-height: 54rpx;
+					text-align: center;
+				}
+			}
+		}
+		
+	}
+	.order-wrap {
+		margin-top: 99rpx;
+	}
+	.link-wrap {
+		margin: 24rpx 0;
+	}
+}
+</style>
---
+++ web/pages/user/sign/sign.vue
@@ -0,0 +1,140 @@
+<template>
+	<view>
+		<view class="bg">
+			<view class="info-wrap">
+				<view class="info u-flex-col">
+					<view class="u-font-40">350</view>
+					<view class="u-font-32">我的积分</view>
+				</view>
+				<view class="u-m-t-24"><u-button type="primary" size="medium">签到</u-button></view>
+			</view>
+		</view>
+		<view class="num-wrap">
+			<view class="num-item">
+				<view class="num-item__num">15次</view>
+				<view class="num-item__title">总计签到</view>
+			</view>
+			<view class="num-item">
+				<view class="num-item__num">15</view>
+				<view class="num-item__title">签到积分</view>
+			</view>
+		</view>
+		<view class="sign-wrap">
+			<view class="sign-list">
+				<u-grid :col="7" :border="false" >
+					<u-grid-item v-for="(item, index) in list" :key="index">
+						<u-icon :name="item.isSign ? 'star-fill' : 'star'" :size="46"></u-icon>
+						<view class="grid-text">{{ item.time }}</view>
+					</u-grid-item>
+				</u-grid>
+			</view>
+		</view>
+		<view class="sign-info">
+			<u-divider color="#000000" :font-size="32">签到规则</u-divider>
+			<view class="sign-info-rule u-m-t-24">
+				<view class="sign-info-rule-item" v-for="(item, index) in rule" :key="index">
+					<text class="u-font-48">●</text>
+					<text class="u-p-l-12">{{ item }}</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			list: [
+				{
+					isSign: false,
+					time: '11.22'
+				},
+				{
+					isSign: false,
+					time: '11.23'
+				},
+				{
+					isSign: false,
+					time: '11.24'
+				},
+				{
+					isSign: false,
+					time: '11.25'
+				},
+				{
+					isSign: true,
+					time: '11.26'
+				},
+				{
+					isSign: false,
+					time: '11.27'
+				},
+				{
+					isSign: false,
+					time: '11.27'
+				}
+			],
+			rule: [
+				'每天只能签到一次',
+				'客户获得签到积分奖励，自领取之日起一年有效期。',
+				'积分最终解释权归国瑞商业购物中心所有。'
+			]
+		};
+	},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+page {
+	.bg {
+		position: relative;
+		width: 100%;
+		height: 500rpx;
+		background-color: #999999;
+	}
+	.info-wrap {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 500rpx;
+		.info {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			color: white;
+			width: 200rpx;
+			height: 200rpx;
+			border-radius: 50%;
+			border: 2rpx solid white;
+		}
+	}
+	.num-wrap {
+		background-color: #000000;
+		display: flex;
+		height: 130rpx;
+		.num-item {
+			flex: 1;
+			text-align: center;
+			color: white;
+			line-height: 65rpx;
+			.num-item__num {
+				font-weight: 600;
+			}
+			.num-item__title {
+			}
+		}
+	}
+	.sign-info {
+		padding: 48rpx 24rpx;
+		background-color: white;
+		.sign-info-rule-item {
+			font-size: 28rpx;
+			font-weight: 500;
+			line-height: 48rpx;
+		}
+	}
+}
+</style>
